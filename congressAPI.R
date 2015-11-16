17e5ceaa3dcda06cdf9314b8a989071a6ebdfa7b
Jordan Poles <jpoles1@gmail.com>
2015-11-15 20:12:16 -0600

    Successfully added fetching of Senate Roll Calls. Updated DB.

--- congressAPI.R
+++ congressAPI.R
@@ -4,12 +4,10 @@ require(jsonlite)
 require(plyr)
 require(RSQLite)
 require(XML)
+require(plyr)
 #Loading in API key from config
 source("config.R")
-#Setup Database Connection
-dbcon = dbConnect(SQLite(), dbname="data.sqlite")
-dbWriteTable(dbcon, "members", data)
-#
+#Helper Functions
 fetchJSON = function(url){
   i = 1;
   while(i<5){
@@ -20,38 +18,44 @@ fetchJSON = function(url){
       return(fromJSON(rawtext))
     }
     else{
-      print("Failed to fetch, trying again in 30 seconds.")
-      Sys.sleep(30);
+      print("Failed to fetch, trying again in 15 seconds.")
+      Sys.sleep(15);
       i=i+1;
     }
   }
   stop("Could not fetch data, timed out.")
 }
 fetchCongressMembers = function(congressNumber=113, chamber="senate", searchparams=""){
-  url = sprintf("http://api.nytimes.com/svc/politics/%s/us/legislative/congress/%s/%s/members.json?%s&api-key=%s", version, congressNumber, chamber, searchparams, apikey)
-  data = fetchJSON(url)$results$members[[1]]
+  dataurl = sprintf("http://api.nytimes.com/svc/politics/%s/us/legislative/congress/%s/%s/members.json?%s&api-key=%s", version, congressNumber, chamber, searchparams, apikey)
+  data = fetchJSON(dataurl)$results$members[[1]]
   return(data)
+}
+tableWriter = function(data, tablename){
+  dbcon = dbConnect(SQLite(), dbname="data.sqlite")
+  dbWriteTable(dbcon, tablename, data, overwrite=TRUE);
+  dbDisconnect(dbcon);
 }
 fetchAllMembers = function(congressRange, chamber="senate"){
   data = data.frame()
   for(num in congressRange){
     set = fetchCongressMembers(num)
-    set$number = num
+    set$congressNumber = num
     data = rbind.fill(data, set)
   }
+  tableWriter(data, "members")
   return(data)
 }
 fetchBills = function(congressNumber=113, chamber="senate", searchparams=""){
-  url = sprintf("http://api.nytimes.com/svc/politics/%s/us/legislative/congress/%s/%s/bills/passed.json?%s&api-key=%s", version, congressNumber, chamber, searchparams, apikey)
-  data = fetchJSON(url)$results
+  dataurl = sprintf("http://api.nytimes.com/svc/politics/%s/us/legislative/congress/%s/%s/bills/passed.json?%s&api-key=%s", version, congressNumber, chamber, searchparams, apikey)
+  data = fetchJSON(dataurl)$results
   return(data) 
 }
 fetchVote = function(congressNumber=113, chamber="senate", sessionNumber = 1, roleNumber=1, searchparams=""){
-  url = sprintf("http://api.nytimes.com/svc/politics/%s/us/legislative/congress/%s/%s/sessions/votes%s/passed.json?%s&api-key=%s", version, congressNumber, chamber, sessionNumber, searchparams, apikey)
-  data = fetchJSON(url)$results
+  dataurl = sprintf("http://api.nytimes.com/svc/politics/%s/us/legislative/congress/%s/%s/sessions/votes%s/passed.json?%s&api-key=%s", version, congressNumber, chamber, sessionNumber, searchparams, apikey)
+  data = fetchJSON(dataurl)$results
   return(data) 
 }
-fetchXML = function(url){
+fetchXML = function(dataurl){
   rawtext = getURL(dataurl);
   data = xmlToList(rawtext)$votes;
   d = ldply(data, function(x) data.frame(x))
@@ -61,6 +65,19 @@ senateRollCall = function(congressNumber=113, sessionNumber=1){
   data = fetchXML(dataurl);
   return(data);
 }
-x = senateRollCall();
-memberData = fetchAllMembers(80:113)
-fetchBills(111)
+fetchSenateRollCalls = function(congressRange, sessionRange){
+  data = data.frame()
+  for(num in congressRange){
+    for(session in sessionRange){
+      set = senateRollCall(num, session)
+      set$congressNumber = num
+      set$session = session
+      data = rbind.fill(data, set)
+    }
+  }
+  tableWriter(data, "senateRollCalls")
+  return(data);
+}
+rollCallData = fetchSenateRollCalls(101:113, 1:2);
+#memberData = fetchAllMembers(80:113)
+#fetchBills(111)
