2db2746c8f704b7e35650710a3d84b5e7854128d
Jordan Poles <jpoles1@gmail.com>
2015-11-17 17:03:14 -0600

    Started working on analysis in Knitr. Probably best to write scripts for analysis, then enter them into the markdown.

--- voteAnalysis.R
+++ voteAnalysis.R
@@ -1,8 +1,15 @@
 #Functions to use in analzying the data
-queryDB = function(query){
-  dbcon = dbConnect(SQLite(), dbname="data.sqlite")
+queryDB = function(query, dbPath="data.sqlite"){
+  dbcon = dbConnect(SQLite(), dbname=paste(projectRoot, dbPath, sep=""))
   res <- dbSendQuery(dbcon, query)
   data <- fetch(res, -1)
   dbClearResult(res)
   return(data)
+}
+#Converts a given congress and session number to a year using the following table
+sesh = read.delim(paste(projectRoot,"data/sessions.tsv", sep=""))
+congressToYear = function(congressNumber, sessionNumber){
+  sessionNumber = as.factor(sessionNumber)
+  year = sesh[sesh$congress==congressNumber,]$session[sessionNumber]
+  return(as.integer(as.character(year)))
 }
