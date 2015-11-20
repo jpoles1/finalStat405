b9b0ab0673603307d9b28e228aaf2ecc7fd7ded5
Jordan Poles <jpoles1@gmail.com>
2015-11-19 19:56:48 -0600

    Added a plot of political party distributions.

--- voteAnalysis.R
+++ voteAnalysis.R
@@ -13,9 +13,3 @@ congressToYear = function(congressNumber, sessionNumber){
   year = sesh[sesh$congress==congressNumber,]$session[sessionNumber]
   return(as.integer(as.character(year)))
 }
-#Scratch space
-partyData = queryDB("SELECT party as Party, count(*) as ct FROM members WHERE party!='ID' GROUP BY party")
-a = ggplot(partyData, aes(x=reorder(Party, ct), y=ct))+geom_bar(stat="identity", fill=c("green", "red", "blue"))+xlab("\nParty")+ylab("Number of Seats Held")+ggtitle("Number of Senate Seats Held By Each Party\nRange from 101st to 113th Congress.")
-partyDataByYear = queryDB("SELECT party as Party, congressNumber, count(*) as ct FROM members WHERE party!='ID' GROUP BY party, congressNumber")
-c = ggplot(partyDataByYear, aes(x=congressNumber, y=ct, fill=Party))+geom_histogram(position="fill", stat="identity", width=1)+xlab("Congress Number\n")+scale_x_continuous(breaks=101:113)+ylab("\nPercentage of Senate Seats")+ggtitle("Senate Seats Held Per Party By Year\n")
-
