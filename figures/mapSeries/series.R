184304cb12979003c30fcb07e2dd4b7e6486c127
Jordan Poles <jpoles1@gmail.com>
2015-12-01 00:42:53 -0600

    Made lots of beautification changes. Fixed up animation.

--- figures/mapSeries/series.R
+++ figures/mapSeries/series.R
@@ -1,8 +1,34 @@
 stateMap = map_data("state")
 congressRange = 101:113
+#Votes With Party
 for(congressNumber in congressRange){
   stateVote = queryDB(sprintf("SELECT state as stateAbrev, avg(votes_with_party_pct) as withParty FROM members WHERE congressNumber=%s GROUP BY state",congressNumber))
   stateVote$state = apply(stateVote, 1, FUN=function(x){stateAbrevToFull(x["stateAbrev"])})
-  ggplot()+geom_map(data=stateMap, map=stateMap, aes(x=long, y=lat, map_id=region), fill="#ffffff", color="grey10")+geom_map(data=stateVote, map=stateMap, aes(fill=withParty, map_id=state), color="grey10")+ggtitle(sprintf("Votes With Party By State\nCongress Number: %s", congressNumber));
-  ggsave(file=sprintf("figures/mapSeries/%s.png", congressNumber), width= 6, height=4)
+  uniScale = seq(60, 100, by=5);
+  ggplot()+
+    geom_map(data=stateMap, map=stateMap, aes(x=long, y=lat, map_id=region), fill="#ffffff", color="grey10")+
+    geom_map(data=stateVote, map=stateMap, aes(fill=withParty, map_id=state), color="grey10")+
+    ggtitle(sprintf("Patterns in Voting With Party By State\nCongress Number: %s", congressNumber))+
+    xlab("")+ylab("")+
+    scale_x_continuous(breaks=NULL)+
+    scale_y_continuous(breaks=NULL)+
+    scale_fill_gradient(name="% Votes w/ Party", limits=c(0,100))
+  ggsave(file=sprintf("../figures/mapSeries/%s.png", congressNumber), width= 6, height=4, dpi=200)
+}
+#Missed Votes
+alldata = queryDB("SELECT state as stateAbrev, avg(missed_votes_pct) as missed, congressNumber FROM members  GROUP BY congressNumber, state ORDER BY congressNumber DESC, missed DESC")
+upperbound = ceiling(mean(alldata$missed)+(2*sd(alldata$missed)))
+for(congressNumber in congressRange){
+  stateVote = queryDB(sprintf("SELECT state as stateAbrev, avg(missed_votes_pct) as missed FROM members WHERE congressNumber=%s GROUP BY state",congressNumber))
+  stateVote$state = apply(stateVote, 1, FUN=function(x){stateAbrevToFull(x["stateAbrev"])})
+  uniScale = seq(60, 100, by=5);
+  ggplot()+
+    geom_map(data=stateMap, map=stateMap, aes(x=long, y=lat, map_id=region), fill="#ffffff", color="grey10")+
+    geom_map(data=stateVote, map=stateMap, aes(fill=missed, map_id=state), color="grey10")+
+    ggtitle(sprintf("Avg. Percentage of Votes Missed by State Senators\nCongress Number: %s\n", congressNumber))+
+    xlab("")+ylab("")+
+    scale_x_continuous(breaks=NULL)+
+    scale_y_continuous(breaks=NULL)+
+    scale_fill_gradient(name="% Missed Votes", limits=c(0,upperbound), na.value="dark red")
+  ggsave(file=sprintf("../figures/mapSeries/missed/%s.png", congressNumber), width= 10, height=6, dpi=200)
 }
