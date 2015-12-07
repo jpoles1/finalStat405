9a22dfe2b496de766a8060b0ff54fa001bb44061
Jordan Poles <jpoles1@gmail.com>
2015-12-06 19:05:03 -0600

    Reorganized data-fetching files to make functions more organized.

---
+++ fetchData.R
@@ -0,0 +1,16 @@
+#Libraries
+require(RCurl)
+require(jsonlite)
+require(plyr)
+require(RSQLite)
+require(XML)
+#Loading in API key from config
+source("config.R")
+source("fetchJSON.R")
+source("fetchXML.R")
+#Fetch session data for determining which years align with a given congress session
+sesh = read.delim("data/sessions.tsv")
+range = 101:113
+memberData = fetchAllMembers(range)
+rollCallData = fetchSenateRollCalls(range, 1:2);
+voteData = fetchVotes(rollCallData);
--- fetchJSON.R
+++ fetchJSON.R
@@ -1,14 +1,3 @@
-#Libraries
-require(RCurl)
-require(jsonlite)
-require(plyr)
-require(RSQLite)
-require(XML)
-#Loading in API key from config
-source("config.R")
-source("senateRollCall.R")
-#Fetch session data for determining which years align with a given congress session
-sesh = read.delim("data/sessions.tsv")
 #Helper Functions
 fetchJSON = function(url){
   i = 1;
@@ -85,7 +74,3 @@ fetchVotes = function(rollCallData){
   try(tableWriter(data, "votes"));
   return(data)
 }
-range = 101:113
-memberData = fetchAllMembers(range)
-rollCallData = fetchSenateRollCalls(range, 1:2);
-voteData = fetchVotes(rollCallData);
